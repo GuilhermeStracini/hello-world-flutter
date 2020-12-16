304883fb6744ace0b73137636fb519417f6975e7
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2020-12-16 08:17:19 -0300

    Start

--- SignalRChat/Hubs/ChatHub.cs
+++ SignalRChat/Hubs/ChatHub.cs
@@ -8,6 +8,7 @@ namespace SignalRChat.Hubs
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
+            System.Console.WriteLine($"User {user} said: {message}");
         }
     }
 }
